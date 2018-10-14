28dbbc14f8fbd78b8d433dbbb88bc7f408615c9f
Priti Gupta <43332924+pritispoc@users.noreply.github.com>
2018-10-14 14:45:41 +0200

    Merge branch 'master' of https://github.com/pritispoc/keyWorks-uppgift-4

--- public_html/js/fade-on-scroll.js
+++ public_html/js/fade-on-scroll.js
@@ -1,91 +1,23 @@
-/*fade on scroll
-autor: Priti
-created october 3 2018
-*/
-// defines how many  pixels  in the viewport breakpoint
-const breakpoint = $(".wrapper").width();
+/* This is the file where we have all javascript code. Each function should have a comment with the name of the author and purpose of the function*/
 
-/// define the initial width of the view port
-let previousWidth = $(window).width();
+function mOver() {
 
-selectAlphaMode(previousWidth);
+       var x = document.getElementById("firstShape");
+       if (x.className === "firstShape") {
+           x.className = "box ";
+       } else {
+           x.className = "triangle-up ";
+       }
+   }
 
-$(window).resize(function(){
-    let width =  $(window).width();
 
-    if((width < breakpoint && previousWidth >= breakpoint) || (width >= breakpoint && previousWidth < breakpoint)) {
-        selectAlphaMode(width);
-    }
-    previousWidth = width;
-});
 
-// toggle scroll bar transparency as per viewport width
-function selectAlphaMode(width) {
-    if (width < breakpoint) {
-        $(document).scroll().off();
-        setAlpha(1);
-    } else {
-        setAlpha(calcAlpha());
-        $(document).scroll(function(){
-        setAlpha(calcAlpha());
-        });
-    }
+function mOut() {
 
+   var x = document.getElementById("firstShape");
+   if (x.className === "firstShape") {
+       x.className = "triangle-up loading";
+   } else {
+       x.className = "box loading";
+   }
 }
-
-// selectAlphaMode(previousWidth);
-
-function setAlpha(alpha) {
-    $("nav").css("background-color","rgba(0, 0, 0, " + alpha + ")");
-}
-
-
-// setAlpha(0.5);
-
-function calcAlpha() {
-
-    let alpha = 0;
-    // defines a variable for how long to go up in the html document
-     // calling the jquery function to pick current scroll location
-    let scroll = $(document).scrollTop();
-    //at which pixel does the fade start begin
-    const fadeStart = 0;
-
-    //at which pixel does the fade stop
-    const fadeStop = 200;
-
-    // difference between start and stop
-    const fadeLength = fadeStop - fadeStart;
-    // transparency level parameter alpha set based on current scroll level 
-    if (scroll < fadeStart) {
-        alpha = 0;
-    } else if (scroll < fadeStop) {
-        alpha = (scroll - fadeStart) / fadeLength;
-    } else { 
-        alpha = 1;
-    }
-    // returns the transparency alpha level based on scroll position
-    return alpha;
-}
-// jquery call for capturing live scroll event
-// $(document).scroll(function() {
-//     setAlpha(calcAlpha());
-// });
-
-function navToggle() {
-    var x = document.getElementById("myTopnav");
-
-    if (x.className === "topnav") {
-        x.className += " dropped";
-        
-    } else {
-        x.className = "topnav";
-    }
-}
-
-
-
-
-
-
-
