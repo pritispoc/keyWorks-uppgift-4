428fb64885beec844598f4ae214c357205a7b4fb
Alvar <alvararias@gmail.com>
2018-10-16 21:29:24 +0200

    oka
    oka

--- public_html/js/fade-on-scroll.js
+++ public_html/js/fade-on-scroll.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
 
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+var slideInterval = setInterval(nextSlide,5000);
 function nextSlide(){
 slides[currentSlide].className = 'slide';
 currentSlide = (currentSlide+1)%slides.length;
